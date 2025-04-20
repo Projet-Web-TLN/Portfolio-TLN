1e10b7ac5085544dc849cae0b6a19b0710751879
Théo Le Nezet <theo.lenezet@gmail.com>
2025-04-21 00:09:45 +0200

    Add first content

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php include 'header.php'; ?>
+
+<body class="page-accueil">
+
+<!----------------------------->
+<!--------- TITRE ------------->
+<!----------------------------->
+
+  <div class="container mt-5 bloc-center">
+      <div class="texte-bg-flou">
+        <p class="titre">Théo Le Nezet</p>
+      </div>
+      <br>
+      <div class="texte-bg-flou">
+        <p class="titre2">Technicien informatique système et réseau</p>
+      </div>
+    </div>
+
+<!----------------------------->
+<!--------- 1 BLOCK ----------->
+<!----------------------------->
+  <div class="contenu-flex raw">
+    <div class="bloc-texte-fond-blanc col-8">
+      <h1>Bienvenue sur mon site</h1>
+      <p>Voici un exemple de texte avec un fond semi-transparent, occupant deux tiers de l'écran.</p>
+    </div>
+    <div class="bloc-image col-4">
+      <img src="image/photo.jpg" alt="Image de présentation">
+    </div>
+  </div>
+
+<!----------------------------->
+<!--------- 2E BLOCK ---------->
+<!----------------------------->
+  <div class="row">
+  <div class="col">col</div>
+  <div class="col">col</div>
+  </div>
+
+<!----------------------------->
+<!--------- 3E BLOCK ---------->
+<!----------------------------->
+
+
+
+
+
+
+  <?php include 'footer.php'; ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
+    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
